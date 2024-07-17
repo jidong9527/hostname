b2e5234da1e869eeaadc1f27f850ab96910c4506
jidongdong <jidongdong@360.cn>
2024-07-17 16:11:10 +0800

    feat(main): 增减headers接口

--- Dockerfile
+++ Dockerfile
@@ -3,7 +3,7 @@ COPY main.go /go
 RUN CGO_ENABLED=0 GO111MODULE="off" go build -o main
 
 FROM centos:7
-RUN yum -y install net-tools bind-utils
+RUN yum -y install net-tools bind-utils mysql telnet nc openssh
 WORKDIR /
 COPY --from=builder /go/main /main
 EXPOSE 80
--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"flag"
 	"fmt"
 	"log"
@@ -9,8 +10,7 @@ import (
 )
 
 var (
-	hostname   string
-	listenAddr string
+	hostname string
 )
 
 func IndexHandle(w http.ResponseWriter, r *http.Request) {
@@ -18,17 +18,43 @@ func IndexHandle(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hostname: %s\n", hostname)
 }
 
-func main() {
-	flag.StringVar(&listenAddr, "listen", ":80", "Server listen address")
+func HeaderHandle(w http.ResponseWriter, r *http.Request) {
+	log.Printf("[%s] %s %s %s\n", hostname, r.RemoteAddr, r.Method, r.URL)
+	header := r.URL.Query().Get("name")
+	if header != "" {
+		fmt.Fprintf(w, "Header %s value: %s\n", header, r.Header.Get(header))
+		return
+	}
+	b, _ := json.Marshal(r.Header)
+	fmt.Fprintf(w, "Headers: %s\n", b)
+}
+
+func parseListen() string {
+	var listenAddr string
+	flag.StringVar(&listenAddr, "listen", "", "Server listen address")
 	flag.Parse()
+	if listenAddr != "" {
+		return listenAddr
+	}
+	listenAddr, _ = os.LookupEnv("WHOAMI_LISTEN")
+	if listenAddr != "" {
+		return listenAddr
+	}
+	return "0.0.0.0:80"
+}
+
+func main() {
 	var err error
 	hostname, err = os.Hostname()
 	if err != nil {
 		log.Fatalln(err)
 	}
 
+	listenAddr := parseListen()
+
 	log.Printf("Http server running on %s \n", listenAddr)
 	http.HandleFunc("/", IndexHandle)
+	http.HandleFunc("/headers", HeaderHandle)
 	if err = http.ListenAndServe(listenAddr, nil); err != nil {
 		log.Fatalln(err)
 	}
